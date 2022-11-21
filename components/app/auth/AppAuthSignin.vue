b7b10f1dbe6bfbab2196be1cd17a8cc55f648947
John Martin <74799962+jhnmartin@users.noreply.github.com>
2022-11-20 19:39:54 -0600

    styling

--- components/app/auth/AppAuthSignin.vue
+++ components/app/auth/AppAuthSignin.vue
@@ -89,7 +89,7 @@
           <div>
             <button
               @click="signIn"
-              class="flex w-full justify-center rounded-md border border-transparent bg-indigo-600 py-2 px-4 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+              class="auth-form-button"
             >
               Sign in
             </button>
